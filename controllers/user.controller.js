2711cb5b5c8243d033cea43ee5acb545b1c511cb
Nguyễn Trần Thắng <51274694+tranthang9111997@users.noreply.github.com>
2021-01-23 23:50:30 +0700

    pc

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -297,8 +297,7 @@ function getcpu(HWModelStr){
     var FK_IMEI = imei_gen();
    var dl= dungluongmacdinh(ProductType);
    var ip=req.body.clientIp;
-   console.log(ip);
-   if(ip==""){
+   if(ip!=="undefined"||ip!==null){
       ip = req.clientIp;
       if (ip.substr(0, 7) == "::ffff:") {
         ip = ip.substr(7)
