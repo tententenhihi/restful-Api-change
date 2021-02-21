f69c012eb2d353fadf70140ccf69977c08ba8abe
Nguyễn Trần Thắng <51274694+tranthang9111997@users.noreply.github.com>
2021-02-21 13:27:07 +0700

    pc

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -245,7 +245,7 @@ exports.SELECT = async (req, res) => {
   console.log(deviceinfo);
   console.log(os);
   console.log("------------");
-  if (deviceinfo == "undefined"&& os == "undefined") {
+  if (deviceinfo === undefined&& os === undefined) {
     sql='SELECT os.OSVersion AS \'ProductVersion\',model.HWModel AS \'HWModelStr\',model.Platform AS \'HardwarePlatform\',os.Build AS \'BuildVersion\',model.HWMachine AS \'ProductType\',model.BDID AS \'BoardId\',os.utsname_Systemversion AS \'Systemversion\',os.UserAgent AS \'UA\',os.utsname_Releasenumber AS \'Releasenumber\',model.CPID,model.CpuFreq,model.nCpu,model.cpufamily,model.MemorySize,model.ScreenHeight,model.ScreenWidth,model.ResolutionHeight,model.ResolutionWidth FROM os,model ORDER BY RAND() LIMIT 1';
   }else{
     sql="SELECT os.OSVersion AS \'ProductVersion\',model.HWModel AS \'HWModelStr\',model.Platform AS \'HardwarePlatform\',os.Build AS \'BuildVersion\',model.HWMachine AS \'ProductType\',model.BDID AS \'BoardId\',os.utsname_Systemversion AS \'Systemversion\',os.UserAgent AS \'UA\',os.utsname_Releasenumber AS \'Releasenumber\',model.CPID,model.CpuFreq,model.nCpu,model.cpufamily,model.MemorySize,model.ScreenHeight,model.ScreenWidth,model.ResolutionHeight,model.ResolutionWidth FROM os,model where os.OSVersion like '%"+os+"%' and model.HWMachine='"+deviceinfo+"' ORDER BY RAND() LIMIT 1";
