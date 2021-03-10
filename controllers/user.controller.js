2ab18d6fe3412f6f6549ede39e3a208e9e5c63e3
Nguyễn Trần Thắng <51274694+tranthang9111997@users.noreply.github.com>
2021-03-10 17:13:28 +0700

    pc

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -337,8 +337,13 @@ exports.SELECT = async (req, res) => {
     path: '/json/' + ip + '?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query&key=DcyaIbvQx69VZNA',
   }
 
- var ipv6=await publicIp.v6(ip);
-
+ var ipv6=(async () => {
+	await publicIp.v6({
+		fallbackUrls: [
+			'https://ifconfig.co/ip'
+		]
+	});
+})();
 console.log(ipv6);
   var rs = await timezone.timezone(options);
   if (rs["status"] == "fail") {
